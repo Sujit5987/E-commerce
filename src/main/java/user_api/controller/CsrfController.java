843c822727d0987dbd3fa5bc3592887c2621b4a1
Sujit5987 <sujitdash5987@gmail.com>
2026-05-03 22:13:12 +0530

    second commit

--- src/main/java/user_api/controller/CsrfController.java
+++ src/main/java/user_api/controller/CsrfController.java
@@ -1,5 +1,6 @@
 package user_api.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,14 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
+@Slf4j
 public class CsrfController {
 
     @GetMapping("/api/csrf-token")
     public ResponseEntity<String> csrfToken(HttpServletRequest request) {
         CsrfToken token = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
-        if (token == null) {
-            return ResponseEntity.noContent().build();
-        }
+        log.debug(token.toString());
         return ResponseEntity.ok(token.getToken());
     }
 }
